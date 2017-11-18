6631a9b67c451b32224c1bdfde1821fb5bf8f73b
Brook Fox <bfox117@gmail.com>
2017-11-18 01:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log("Expected the name to be Susan")
+} else if {
+  
 }
-
 
 // Don't worry about this
 module.exports = { name, height
